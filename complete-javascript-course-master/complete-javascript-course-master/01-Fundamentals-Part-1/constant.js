8aff328c6f61f332088cfcf523e8060956b3937a
V4nshaj <vanshajbarnwal21@gmail.com>
2024-01-31 06:05:28 +0530

    prompt function added

--- complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/constant.js
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/constant.js
@@ -1,6 +1,6 @@
 let age = 30;
 age = 31;
 const year =1989;
-year=2024; //will not change shows error in console bcz constant
+// year=2024; //will not change shows error in console bcz constant
 // const job;//error as constant variable need to be initialized
 //always try to use const variable 
---
+++ complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/prompt.js
@@ -0,0 +1,15 @@
+const favourite = prompt("What's your favourite number?");
+console.log(favourite);
+console.log(typeof favourite);
+
+if(favourite == 23) //'23' == 23
+{
+    console.log("Cool 23");
+}
+const page = Number(prompt("What is ur page"));
+if(page === 40) //40 == 40
+{
+    console.log("40 page no.");
+}
+//strict not equal to
+if(page !== 40) console.log('Why not 40')
