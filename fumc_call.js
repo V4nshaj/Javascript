337b8ff418b9a343366740752c15b00ef4ed4de9
V4nshaj <vanshajbarnwal21@gmail.com>
2024-02-02 23:22:31 +0530

    Function call

---
+++ fumc_call.js
@@ -0,0 +1,11 @@
+//function that cut fruit in multiple pieces
+function cutFruitPieces(fruit){
+    return fruit *4;
+}
+function fruitProcessor(apples, oranges)
+{
+    const applePieces
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
